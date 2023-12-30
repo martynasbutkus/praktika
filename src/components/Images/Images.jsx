270c4abe7988103629ddfa529b85c1132a513915
martyasbutkus <noobtuberis1990@gmail.com>
2023-12-30 15:43:18 +0200

    asd

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -3,8 +3,8 @@ import './Images.css'
 const Images = ({imagesrc, text}) =>{
     return (
       <div className='image'>
-      <img src={imagesrc} alt="" />
-      <h1>{text}</h1>
+      <img src={imagesrc ? imagesrc : "https://vilniausgalerija.lt/wp-content/uploads/2020/06/marco-xu-ToUPBCO62Lw-unsplash.jpg"} alt="" />
+      <h1>{text ? text : "Nuotrauka"}</h1>
       </div>
     )
   }
